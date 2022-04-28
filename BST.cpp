ea6b8b98e7b366af93bc9ca149f2a15501ac8db7
swishcodez <104043690+swishcodez@users.noreply.github.com>
2022-04-28 11:40:25 +0800

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -1,94 +1,120 @@
 #include <iostream>
 using namespace std;
 
-struct node {
-  int key;
-  struct node *left, *right;
+struct node
+{
+    int key;
+    struct node *left, *right;
 };
 
-struct node *newNode(int item) {
-  struct node *temp = (struct node *)malloc(sizeof(struct node));
-  temp->key = item;
-  temp->left = temp->right = NULL;
-  return temp;
+struct node *newNode(int item)
+{
+    struct node *temp = (struct node *)malloc(sizeof(struct node));
+    temp->key = item;
+    temp->left = temp->right = NULL;
+    return temp;
 }
 
-void inorder(struct node *root) {
-  if (root != NULL) {
+void inorder(struct node *root, int space = 0, int height = 10)
+{
+    if (root != NULL)
+    {
+        space += height;
+        inorder(root->right, space);
+        cout << endl;
+        for (int i = height; i < space; i++)
+        {
+            cout << ' ';
+        }
+        cout << root->key;
 
-    inorder(root->left);
-
-    cout << root->key << " -> ";
-
-    inorder(root->right);
-  }
-}
-
-struct node *insert(struct node *node, int key) {
-
-  if (node == NULL) return newNode(key);
-
-  if (key < node->key)
-    node->left = insert(node->left, key);
-  else
-    node->right = insert(node->right, key);
-
-  return node;
-}
-
-struct node *minValueNode(struct node *node) {
-  struct node *current = node;
-
-  while (current && current->left != NULL)
-    current = current->left;
-
-  return current;
-}
-
-struct node *deleteNode(struct node *root, int key) {
-
-  if (root == NULL) return root;
-
-  if (key < root->key)
-    root->left = deleteNode(root->left, key);
-  else if (key > root->key)
-    root->right = deleteNode(root->right, key);
-  else {
-    if (root->left == NULL) {
-      struct node *temp = root->right;
-      free(root);
-      return temp;
-    } else if (root->right == NULL) {
-      struct node *temp = root->left;
-      free(root);
-      return temp;
+        cout << endl;
+        inorder(root->left, space);
     }
-// ^4 sariin 28 delete node
-    struct node *temp = minValueNode(root->right);
-
-    root->key = temp->key;
-
-    root->right = deleteNode(root->right, temp->key);
-  }
-  return root;
+    else
+    {
+        return;
+    }
 }
 
-int main() {
-  struct node *root = NULL;
-  root = insert(root, 8);
-  root = insert(root, 3);
-  root = insert(root, 1);
-  root = insert(root, 6);
-  root = insert(root, 7);
-  root = insert(root, 10);
-  root = insert(root, 14);
-  root = insert(root, 4);
+struct node *insert(struct node *node, int key)
+{
 
-  cout << "Inorder traversal: ";
-  inorder(root);
+    if (node == NULL)
+    {
+        return newNode(key);
+    };
+    if (key < node->key)
+    {
+        node->left = insert(node->left, key);
+    }
+    else
+        node->right = insert(node->right, key);
 
-  cout << "\nAfter deleting 10\n";
-  root = deleteNode(root, 10);
-  cout << "Inorder traversal: ";
-  inorder(root);
+    return node;
+}
+
+struct node *minValueNode(struct node *node)
+{
+    struct node *current = node;
+
+    while (current && current->left != NULL)
+        current = current->left;
+
+    return current;
+}
+
+struct node *deleteNode(struct node *root, int key)
+{
+
+    if (root == NULL)
+        return root;
+
+    if (key < root->key)
+        root->left = deleteNode(root->left, key);
+    else if (key > root->key)
+        root->right = deleteNode(root->right, key);
+    else
+    {
+        if (root->left == NULL)
+        {
+            struct node *temp = root->right;
+            free(root);
+            return temp;
+        }
+        else if (root->right == NULL)
+        {
+            struct node *temp = root->left;
+            free(root);
+            return temp;
+        }
+        // ^4 sariin 28 delete node
+        struct node *temp = minValueNode(root->right);
+
+        root->key = temp->key;
+
+        root->right = deleteNode(root->right, temp->key);
+    }
+    return root;
+}
+
+int main()
+{
+    struct node *root = NULL;
+    root = insert(root, 8);
+    root = insert(root, 3);
+    root = insert(root, 1);
+    root = insert(root, 6);
+    root = insert(root, 7);
+    root = insert(root, 10);
+    root = insert(root, 14);
+    root = insert(root, 4);
+
+    cout << "Inorder traversal: ";
+    inorder(root);
+
+    cout << "\nAfter deleting 10\n";
+    root = deleteNode(root, 10);
+    cout << "Inorder traversal: ";
+    inorder(root);
 }
